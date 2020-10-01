74510e1187b7b6ff175e3210b14d1987dc591cf0
Jean Laroche <jean.laroche0@gmail.com>
2020-10-01 15:42:39 -0700

    Start implementing printing of invest trans

--- ofxparse/ofxprinter.py
+++ ofxparse/ofxprinter.py
@@ -48,15 +48,17 @@ class OfxPrinter():
             strdt_msec = strdt_msec[:msec_digs]
         return strdt + '.' + strdt_msec
 
-    def writeInvestTrn(self, trn, tabs=5):
-        self.writeLine("<STMTTRN>", tabs=tabs)
-        tabs += 1
+    def writeInvestTrn(self, trn, tabs=0):
+        self.writeLine("<{}>".format(trn.type.upper()), tabs=tabs)
 
-        self.writeLine("<TRNTYPE>{}".format(trn.type.upper()), tabs=tabs)
+        self.writeLine("<INVTRAN>", tabs=tabs)
+        self.writeLine("<FITID>{}".format(trn.id), tabs=tabs)
         self.writeLine("<DTSETTLE>{}".format(self.printDate(trn.settleDate)), tabs=tabs)
         self.writeLine("<DTTRADE>{}".format(self.printDate(trn.tradeDate)), tabs=tabs)
+        if len(trn.memo.strip()) > 0:
+            self.writeLine("<MEMO>{}".format(trn.memo), tabs=tabs)
+        self.writeLine("</INVTRAN>", tabs=tabs)
 
-        self.writeLine("<FITID>{}".format(trn.id), tabs=tabs)
         self.writeLine("<INCOMETYPE>{}".format(trn.income_type), tabs=tabs)
         self.writeLine("<UNITS>{}".format(float(trn.units)), tabs=tabs)
         self.writeLine("<UNITPRICE>{}".format(float(trn.unit_price)), tabs=tabs)
@@ -64,18 +66,31 @@ class OfxPrinter():
         self.writeLine("<FEES>{}".format(float(trn.fees)), tabs=tabs)
         self.writeLine("<TOTAL>{}".format(float(trn.total)), tabs=tabs)
         self.writeLine("<TFERACTION>{}".format(trn.tferaction), tabs=tabs)
+        self.writeLine("</{}>".format(trn.type.upper()), tabs=tabs)
 
-        if len(trn.memo.strip()) > 0:
-            self.writeLine("<MEMO>{}".format(trn.memo), tabs=tabs)
+    def writeInvestPos(self, trn, tabs=0):
+        self.writeLine("<POSMF>", tabs=tabs)
+        self.writeLine("<INVPOS>", tabs=tabs)
+        self.writeLine("<SECID>", tabs=tabs)
+        self.writeLine("<UNIQUEID>{}".format(trn.security), tabs=tabs)
+        self.writeLine("<UNIQUEIDTYPE>CUSIP", tabs=tabs)
+        self.writeLine("</SECID>", tabs=tabs)
 
-        tabs -= 1
-        self.writeLine("</STMTTRN>", tabs=tabs)
+        self.writeLine("<DTPRICEASOF>{}".format(self.printDate(trn.date)), tabs=tabs)
+        self.writeLine("<UNITS>{}".format(float(trn.units)), tabs=tabs)
+        self.writeLine("<UNITPRICE>{}".format(float(trn.unit_price)), tabs=tabs)
+        self.writeLine("<MKTVALMKTVAL>{}".format(float(trn.market_value)), tabs=tabs)
+        self.writeLine("</POSMF>", tabs=tabs)
+        self.writeLine("</INVPOS>", tabs=tabs)
+
+        # tabs -= 1
 
     def writeTrn(self, trn, tabs=5):
         self.writeLine("<STMTTRN>", tabs=tabs)
-        tabs += 1
+        # tabs += 1
 
         self.writeLine("<TRNTYPE>{}".format(trn.type.upper()), tabs=tabs)
+
         self.writeLine("<DTPOSTED>{}".format(
             self.printDate(trn.date)
         ), tabs=tabs)
@@ -93,7 +108,7 @@ class OfxPrinter():
         if len(trn.memo.strip()) > 0:
             self.writeLine("<MEMO>{}".format(trn.memo), tabs=tabs)
 
-        tabs -= 1
+        # tabs -= 1
         self.writeLine("</STMTTRN>", tabs=tabs)
 
     def writeLedgerBal(self, statement, tabs=4):
@@ -105,7 +120,7 @@ class OfxPrinter():
             self.writeLine("<BALAMT>{0:.2f}".format(float(bal)), tabs=tabs+1)
             self.writeLine("<DTASOF>{0}".format(
                 self.printDate(baldt)
-            ), tabs=tabs+1)
+            ), tabs=tabs)
             self.writeLine("</LEDGERBAL>", tabs=tabs)
 
     def writeAvailBal(self, statement, tabs=4):
@@ -117,12 +132,12 @@ class OfxPrinter():
             self.writeLine("<BALAMT>{0:.2f}".format(float(bal)), tabs=tabs+1)
             self.writeLine("<DTASOF>{0}".format(
                 self.printDate(baldt)
-            ), tabs=tabs+1)
+            ), tabs=tabs)
             self.writeLine("</AVAILBAL>", tabs=tabs)
 
     def writeBnkAcctStmTrs(self,tabs,acct):
         self.writeLine("<STMTRS>", tabs=tabs)
-        tabs += 1
+        # tabs += 1
 
         if acct.curdef:
             self.writeLine("<CURDEF>{0}".format(
@@ -134,19 +149,19 @@ class OfxPrinter():
             if acct.routing_number:
                 self.writeLine("<BANKID>{0}".format(
                     acct.routing_number
-                ), tabs=tabs + 1)
+                ), tabs=tabs)
             if acct.account_id:
                 self.writeLine("<ACCTID>{0}".format(
                     acct.account_id
-                ), tabs=tabs + 1)
+                ), tabs=tabs)
             if acct.account_type:
                 self.writeLine("<ACCTTYPE>{0}".format(
                     acct.account_type
-                ), tabs=tabs + 1)
+                ), tabs=tabs)
             self.writeLine("</BANKACCTFROM>", tabs=tabs)
 
         self.writeLine("<BANKTRANLIST>", tabs=tabs)
-        tabs += 1
+        # tabs += 1
         self.writeLine("<DTSTART>{0}".format(
             self.printDate(acct.statement.start_date)
         ), tabs=tabs)
@@ -157,61 +172,81 @@ class OfxPrinter():
         for trn in acct.statement.transactions:
             self.writeTrn(trn, tabs=tabs)
 
-        tabs -= 1
+        # tabs -= 1
 
         self.writeLine("</BANKTRANLIST>", tabs=tabs)
 
         self.writeLedgerBal(acct.statement, tabs=tabs)
         self.writeAvailBal(acct.statement, tabs=tabs)
 
-        tabs -= 1
+        # tabs -= 1
 
         self.writeLine("</STMTRS>", tabs=tabs)
 
     def writeInvestAcctStmTrs(self,tabs,acct):
-        self.writeLine("<STMTRS>", tabs=tabs)
-        tabs += 1
+        tabs = 0
+        self.writeLine("<INVSTMTRS>", tabs=tabs)
+        # tabs += 1
+
+        if acct.account_id:
+            self.writeLine("<ACCTID>{0}".format(
+                acct.account_id
+            ), tabs=tabs)
+        if acct.account_type:
+            self.writeLine("<ACCTTYPE>{0}".format(
+                acct.account_type
+            ), tabs=tabs)
 
         if acct.curdef:
             self.writeLine("<CURDEF>{0}".format(
                 acct.curdef
             ), tabs=tabs)
 
-        self.writeLine("<INVSTMTRS>", tabs=tabs)
-        tabs += 1
-        # self.writeLine("<DTSTART>{0}".format(
-        #     self.printDate(acct.statement.start_date)
-        # ), tabs=tabs)
-        # self.writeLine("<DTEND>{0}".format(
-        #     self.printDate(acct.statement.end_date)
-        # ), tabs=tabs)
-
+        self.writeLine("<INVTRANLIST>", tabs=tabs)
+        self.writeLine("<DTSTART>{0}".format(
+            self.printDate(acct.statement.start_date)
+        ), tabs=tabs)
+        self.writeLine("<DTEND>{0}".format(
+            self.printDate(acct.statement.end_date)
+        ), tabs=tabs)
+        tabs = 0
         for trn in acct.statement.transactions:
             self.writeInvestTrn(trn, tabs=tabs)
+        self.writeLine("</INVTRANLIST>", tabs=tabs)
 
-        tabs -= 1
-
-        self.writeLine("</INVSTMTRS>", tabs=tabs)
+        # WHAT ABOUT POSITIONS?
+        self.writeLine("<INVPOSLIST>", tabs=tabs)
+        for trn in acct.statement.positions:
+            self.writeInvestPos(trn, tabs=tabs)
+        self.writeLine("</INVPOSLIST>", tabs=tabs)
 
         self.writeLedgerBal(acct.statement, tabs=tabs)
         self.writeAvailBal(acct.statement, tabs=tabs)
 
-        tabs -= 1
+        # tabs -= 1
 
-        self.writeLine("</STMTRS>", tabs=tabs)
+        self.writeLine("</INVSTMTRS>", tabs=tabs)
 
-    def writeStmTrs(self, tabs=3):
+    def writeStmTrs(self, tabs=0):
         for acct in self.ofx.accounts:
             if acct.type == 1:
                 self.writeBnkAcctStmTrs(tabs,acct)
             if acct.type == 3:
+                if not len(acct.statement.transactions): continue
                 self.writeInvestAcctStmTrs(tabs,acct)
+                # todo
+                break
 
-    def writeBankMsgsRsv1(self, tabs=1):
-        self.writeLine("<BANKMSGSRSV1>", tabs=tabs)
-        tabs += 1
-        self.writeLine("<STMTTRNRS>", tabs=tabs)
-        tabs += 1
+    def writeBankMsgsRsv1(self, tabs=0):
+        if self.ofx.account.type == 1:
+            self.writeLine("<BANKMSGSRSV1>", tabs=tabs)
+            # tabs += 1
+            self.writeLine("<STMTTRNRS>", tabs=tabs)
+        elif self.ofx.account.type == 3:
+            self.writeLine("<INVSTMTMSGSRSV1>", tabs=tabs)
+        # tabs += 1
+            self.writeLine("<INVSTMTTRNRS>", tabs=tabs)
+        # tabs += 1
         if self.ofx.trnuid is not None:
             self.writeLine("<TRNUID>{0}".format(
                 self.ofx.trnuid
@@ -220,23 +255,29 @@ class OfxPrinter():
             self.writeLine("<STATUS>", tabs=tabs)
             self.writeLine("<CODE>{0}".format(
                 self.ofx.status['code']
-            ), tabs=tabs+1)
+            ), tabs=tabs)
             self.writeLine("<SEVERITY>{0}".format(
                 self.ofx.status['severity']
-            ), tabs=tabs+1)
+            ), tabs=tabs)
             self.writeLine("</STATUS>", tabs=tabs)
         self.writeStmTrs(tabs=tabs)
-        tabs -= 1
-        self.writeLine("</STMTTRNRS>", tabs=tabs)
-        tabs -= 1
-        self.writeLine("</BANKMSGSRSV1>", tabs=tabs)
+        if self.ofx.account.type == 1:
+            # tabs -= 1
+            self.writeLine("</STMTTRNRS>", tabs=tabs)
+            # tabs -= 1
+            self.writeLine("</BANKMSGSRSV1>", tabs=tabs)
+        elif self.ofx.account.type == 3:
+            # tabs -= 1
+            self.writeLine("</INVSTMTMSGSRSV1>", tabs=tabs)
+            # tabs -= 1
+            self.writeLine("</INVSTMTTRNRS>", tabs=tabs)
 
     def writeOfx(self, tabs=0):
         self.writeLine("<OFX>", tabs=tabs)
-        tabs += 1
+        # tabs += 1
         self.writeSignOn(tabs=tabs)
         self.writeBankMsgsRsv1(tabs=tabs)
-        tabs -= 1
+        # tabs -= 1
         # No newline at end of file
         self.writeLine("</OFX>", tabs=tabs, term="")
 
